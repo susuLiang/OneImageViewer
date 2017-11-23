f24a7d94fa713fba779d5cffa54e7a2834b6ef7e
Susu Liang <sophie790709@gmail.com>
2017-11-23 15:30:25 +0800

    Can pick image from user album

--- OneImageViewer/ViewController.swift
+++ OneImageViewer/ViewController.swift
@@ -8,36 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIScrollViewDelegate {
+class ViewController: UIViewController, UIScrollViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     var scrollView: UIScrollView!
-    var imageView: UIImageView!
+    @IBOutlet weak var pickButton: UIButton!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var imageViewPicked: UIImageView!
+    var imagePicker: UIImagePickerController!
+   
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setView()
-        
-//        imageView = UIImageView(image: UIImage(named: "preview"))
-//
-//        //設定滾動區域及大小
-//        scrollView = UIScrollView(frame: view.bounds)
-//        scrollView.contentSize = imageView.bounds.size
-//
-//        view.addSubview(scrollView)
-//        scrollView.addSubview(imageView)
-//
-//        //當裝置旋轉時，會重新調整大小
-//        scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-//
-//        //1.將滾動區域的位置從原點在左上角改為(1000, 450)
-//        scrollView.contentOffset = CGPoint(x: 1000, y: 450)
-//
-//        //2. 縮放功能需要指定delegate self 跟縮放比例
-//        scrollView.delegate = self
-//        //        scrollView.minimumZoomScale = 0.1
-//        //        scrollView.maximumZoomScale = 4.0
-//        //        scrollView.zoomScale = 1.0
-//        // Do any additional setup after loading the view, typically from a nib.
+        setButtonView()
+        setScrollView()
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -45,7 +29,7 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func setView() {
+    func setButtonView() {
         let yellowView = UIView()
         yellowView.frame = CGRect(x: 0, y: 590, width: self.view.frame.width, height: 77)
         yellowView.backgroundColor = UIColor(red: 249/255.0, green: 223/255.0, blue: 23/255.0, alpha: 1)
@@ -59,45 +43,84 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         pickButton.titleLabel?.font = UIFont(name: "SFUIText", size: 20)
         pickButton.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .heavy)
         yellowView.addSubview(pickButton)
+        pickButton.addTarget(self, action: #selector(self.loadImage(_:)), for: .touchUpInside)
         
         
     }
+    
+    @objc func loadImage(_ sender: UIButton) {
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    
+    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            let imageViewPicked = UIImageView()
+            imageViewPicked.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height - 77)
+            
+            imageViewPicked.contentMode = .scaleAspectFit
+            imageViewPicked.image = pickedImage
+            
+            scrollView.contentSize = imageViewPicked.bounds.size
+            
+            scrollView.addSubview(imageViewPicked)
+            
+            dismiss(animated: true, completion: nil)
+        }
+        
+    }
+    
+    func setScrollView() {
 
-//    // 2.加了縮放功能 protocol (UIScrollViewDelegate) 需要implement 的function
-//    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-//        return imageView
-//    }
-//
-//    //3. 為了讓圖片縮小填滿且有Aspect Fit
+        scrollView = UIScrollView()
+        scrollView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height - 77)
+//        scrollView.contentSize = imageViewPicked.bounds.size
+        scrollView.backgroundColor = UIColor.black
+
+        self.view.addSubview(scrollView)
+//        scrollView.addSubview(imageViewPicked)
+
+        scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+        scrollView.delegate = self
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 4.0
+        scrollView.zoomScale = 1.0
+    }
+
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageViewPicked
+    }
+
 //    fileprivate func updateMinZoomScaleForSize(_ size: CGSize) {
-//        let widthScale = size.width / imageView.bounds.width
-//        let heightScale = size.height / imageView.bounds.height
+//        let widthScale = size.width / imageViewPicked.bounds.width
+//        let heightScale = size.height / imageViewPicked.bounds.height
 //
 //        let minScale = min(widthScale, heightScale)
 //        scrollView.minimumZoomScale = minScale
-//
 //        scrollView.zoomScale = minScale
 //
 //    }
-//
-//    //3. 呼叫
-//    override func viewWillLayoutSubviews() {
-//        super.viewDidLayoutSubviews()
-//
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
 //        updateMinZoomScaleForSize(view.bounds.size)
-//
-//    }
-//
-//    //4.讓圖片置中, 每次縮放之後會被呼叫
-//    func scrollViewDidZoom(_ scrollView: UIScrollView) {
-//        let imageViewSize = imageView.frame.size
-//        let scrollViewSize = scrollView.bounds.size
-//
-//        let verticalPadding = imageViewSize.height < scrollViewSize.height ? (scrollViewSize.height - imageViewSize.height) / 2 : 0
-//        let horizontalPadding = imageViewSize.width < scrollViewSize.width ? (scrollViewSize.width - imageViewSize.width) / 2 : 0
-//
-//        scrollView.contentInset = UIEdgeInsets(top: verticalPadding, left: horizontalPadding, bottom: verticalPadding, right: horizontalPadding)
-//    }
+    }
+
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        let imageViewSize = imageViewPicked.frame.size
+        let scrollViewSize = scrollView.bounds.size
+
+        let verticalPadding = imageViewSize.height < scrollViewSize.height ? (scrollViewSize.height - imageViewSize.height) / 2 : 0
+        let horizontalPadding = imageViewSize.width < scrollViewSize.width ? (scrollViewSize.width - imageViewSize.width) / 2 : 0
+
+        scrollView.contentInset = UIEdgeInsets(top: verticalPadding, left: horizontalPadding, bottom: verticalPadding, right: horizontalPadding)
+    }
     
 }
 
